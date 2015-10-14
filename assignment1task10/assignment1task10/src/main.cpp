410204288f1b47ffec384089cb735afc90c4c82b
Ryang <green-1210@hotmail.com>
2015-10-14 14:28:59 -0700

    seems encrypt function works

--- assignment1task10/assignment1task10/src/main.cpp
+++ assignment1task10/assignment1task10/src/main.cpp
@@ -1,9 +1,5 @@
 //============================================================================
-// Name        : tt.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Name        : assignment1task10.cpp
 //============================================================================
 
 #include <openssl/conf.h>
@@ -14,6 +10,7 @@
 #include <fstream>
 using namespace std;
 
+enum {ecb = 0, cbc = 1};
 
 void handleErrors(void)
 {
@@ -29,12 +26,12 @@ int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *cipherte
 	int ciphertext_len;
 	if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
 
-	if(type == 1){
+	if(type == cbc){
 		if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, NULL)) handleErrors();
 		if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len)) handleErrors();
 		ciphertext_len = len;
 	}
-	else if (type == 0){
+	else if (type == ecb){
 		if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_ecb(), NULL, key, NULL)) handleErrors();
 		if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len)) handleErrors();
 		ciphertext_len = len;
@@ -48,17 +45,17 @@ int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *cipherte
 int main (int argc, char **argv)
 {
 	if (argc < 6) {
-		cout << "Usage: tt -in file -k key -ecb[-cbc]\n";
+		cout << "Usage: cryptotool -in file -k key -ecb[-cbc]\n";
 		return 0;
 	} else if (strcmp(argv[1], "-in") != 0) {
-		cout << "Usage: tt -in file -k key -ecb[-cbc]\n";
+		cout << "Usage: cryptotool -in file -k key -ecb[-cbc]\n";
 		return 0;
 	} else if (strcmp (argv[3], "-k") != 0){
-		cout << "Usage: tt -in file -k key -ecb[-cbc]\n";
+		cout << "Usage: cryptotool -in file -k key -ecb[-cbc]\n";
 		return 0;
 	} else if (strcmp (argv[5], "-cbc") != 0 || strcmp (argv[5], "-ecb") != 0) {
 		if (strcmp (argv[5], "-cbc") != 0 && strcmp (argv[5], "-ecb") != 0) {
-			cout << "Usage: tt -in file -k key -ecb[-cbc]\n";
+			cout << "Usage: cryptotool -in file -k key -ecb[-cbc]\n";
 			return 0;
 		}
 	}
@@ -66,20 +63,19 @@ int main (int argc, char **argv)
 
 	int type;
 	if(strcmp (argv[5], "-cbc") == 0){
-		type = 1;
+		type = cbc;
 	}
 	else{
-		type = 0;
+		type = ecb;
 	}
 
 	unsigned char *key = (unsigned char *) argv[4];
+
 
 	ifstream myfile;
 
 	myfile.open (argv[2]);
 	if (myfile.is_open()){
-
-
 		string line;
 		string lines;
 		while ( getline (myfile,line) ){
@@ -90,22 +86,31 @@ int main (int argc, char **argv)
 
 		int length = lines.length()*sizeof(char);
 		char *plaintext = new char[length];
-		//cout << lines;
 		const char *plaintext_tmp = lines.c_str();
 		strcpy(plaintext,plaintext_tmp);
 
-
-		cout << plaintext;
-
-		unsigned char *ciphertext;
+		unsigned char ciphertext[1000] = "";
 		unsigned char * plaintext1 = (unsigned char*)plaintext;
-		int plaintext_len = strlen(plaintext);
+		int plaintext_len = strlen((char *)plaintext1);
+
+//		cout << "-----------" <<endl;
+//		cout << "plaintext1" << plaintext1 << endl;
+//		cout << "plaintext_len" << plaintext_len << endl;
+//		cout << "ciphertext" << ciphertext << endl;
+//		cout << "key" << key << endl;
+//		cout << "type" << type << endl;
+
 		encrypt(plaintext1, plaintext_len, ciphertext, key, type);
+		cout << "ciphertext:" <<endl;
 		cout << ciphertext;
+
 		myfile.close();
+		delete[] plaintext1;
+
 	} else {
 		cout << "Please specify an existing file.\n";
 	}
+
 
 	return 0;
 }
