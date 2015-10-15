05a3b4ee67fdd07931b8cf85ff48559154b771c1
Ryang <green-1210@hotmail.com>
2015-10-14 19:49:31 -0700

    added: -e/-d and output file name

--- assignment1task10/assignment1task10/src/main.cpp
+++ assignment1task10/assignment1task10/src/main.cpp
@@ -44,8 +44,16 @@ int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *cipherte
 
 int main (int argc, char **argv)
 {
-	if (argc < 6) {
-		cout << "Usage: cryptotool -in file -k key -ecb[-cbc]\n";
+	//argv[1] -in
+	//argv[2] input filename
+	//argv[3] -k option
+	//argv[4] key
+	//argv[5] -ebc or -cbc
+	//argv[6] -e or -d
+	//argb[7] output filename
+
+	if (argc < 8) {
+		cout << "Usage: cryptotool -in input -k key -ecb[-cbc] -e[-d] output\n";
 		return 0;
 	} else if (strcmp(argv[1], "-in") != 0) {
 		cout << "Usage: cryptotool -in file -k key -ecb[-cbc]\n";
@@ -55,6 +63,11 @@ int main (int argc, char **argv)
 		return 0;
 	} else if (strcmp (argv[5], "-cbc") != 0 || strcmp (argv[5], "-ecb") != 0) {
 		if (strcmp (argv[5], "-cbc") != 0 && strcmp (argv[5], "-ecb") != 0) {
+			cout << "Usage: cryptotool -in file -k key -ecb[-cbc]\n";
+			return 0;
+		}
+	} else if (strcmp (argv[6], "-e") != 0 || strcmp (argv[6], "-d") != 0) {
+		if (strcmp (argv[6], "-e") != 0 && strcmp (argv[6], "-d") != 0) {
 			cout << "Usage: cryptotool -in file -k key -ecb[-cbc]\n";
 			return 0;
 		}
@@ -72,50 +85,55 @@ int main (int argc, char **argv)
 	unsigned char *key = (unsigned char *) argv[4];
 
 
-	ifstream myfile;
+	if(strcmp(argv[6], "-e") == 0){
+		//encryption:
+		ifstream myfile;
+		myfile.open (argv[2]);
 
-	myfile.open (argv[2]);
-	if (myfile.is_open()){
-		string line;
-		string lines;
-		while ( getline (myfile,line) ){
-		  //cout << line << '\n';
-		  lines += line;
-		  lines += "\n";
+		if (myfile.is_open()){
+			string line;
+			string lines;
+			while ( getline (myfile,line) ){
+			  lines += line;
+			  lines += "\n";
+			}
+
+			int length = lines.length()*sizeof(char);
+			char *plaintext = new char[length];
+			const char *plaintext_tmp = lines.c_str();
+			strcpy(plaintext,plaintext_tmp);
+
+			unsigned char * plaintext1 = (unsigned char*)plaintext;
+
+//			cout << plaintext1;
+
+			int plaintext_len = strlen((char *)plaintext1);
+
+			unsigned char * ciphertext = new unsigned char[10];
+
+			encrypt(plaintext1, plaintext_len, ciphertext, key, type);
+			cout << ciphertext;
+
+			ofstream fout;
+			fout.open(argv[7]);
+			fout << ciphertext;
+			fout.close();
+
+			//myfile.close();
+			delete[] plaintext1;
+
+		}
+			else {
+			cout << "Please specify an existing file.\n";
 		}
 
-		int length = lines.length()*sizeof(char);
-		char *plaintext = new char[length];
-		const char *plaintext_tmp = lines.c_str();
-		strcpy(plaintext,plaintext_tmp);
+	}//end of encryption
 
-		unsigned char ciphertext[1000] = "";
-		unsigned char * plaintext1 = (unsigned char*)plaintext;
-		int plaintext_len = strlen((char *)plaintext1);
+	else{
+		//decryption
 
-//		cout << "-----------" <<endl;
-//		cout << "plaintext1" << plaintext1 << endl;
-//		cout << "plaintext_len" << plaintext_len << endl;
-//		cout << "ciphertext" << ciphertext << endl;
-//		cout << "key" << key << endl;
-//		cout << "type" << type << endl;
 
-		encrypt(plaintext1, plaintext_len, ciphertext, key, type);
-
-//		cout << ciphertext;
-
-		ofstream fout;
-		fout.open("ciphertext.txt");
-		fout << ciphertext;
-		fout.close();
-
-		myfile.close();
-		delete[] plaintext1;
-
-	} else {
-		cout << "Please specify an existing file.\n";
 	}
-
 
 	return 0;
 }
