9a6e59600504386de63b5c7318b656a7caeaf86f
Ryang <green-1210@hotmail.com>
2015-10-16 15:40:49 -0700

    to let it work fine, i deleted something...

--- assignment1task10/cryptotool/src/main.cpp
+++ assignment1task10/cryptotool/src/main.cpp
@@ -175,76 +175,102 @@
 // Name        : assignment1task10.cpp
 //============================================================================
 
+//
 #include <openssl/conf.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
 #include <string.h>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 using namespace std;
-
 enum {ecb = 0, cbc = 1};
+
 
 void handleErrors(void)
 {
-	ERR_print_errors_fp(stderr);
-	abort();
+  ERR_print_errors_fp(stderr);
+  abort();
 }
 
-
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *ciphertext, unsigned char *key, int type)
+int encrypt(unsigned char *plaintext, int p_len, unsigned char *key, unsigned char *ciphertext, int type)
 {
 	EVP_CIPHER_CTX *ctx;
 	int len;
-	int ciphertext_len;
+	int c_len;
+
+
 	if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
 
-
 	if(type == cbc){
-		cout << "CBC" <<endl;
-		if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, NULL)) handleErrors();
-		if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len)) handleErrors();
-		ciphertext_len = len;
-		if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
-		ciphertext_len += len;
-	}
-	else if (type == ecb){
-		cout << "ECB" <<endl;
-		if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_ecb(), NULL, key, NULL)) handleErrors();
-		if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len)) handleErrors();
-		ciphertext_len = len;
+	  if(EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, NULL) != 1){
+		  handleErrors();
+	  }
 	}
 
-	return ciphertext_len;
+	else if(type == ecb){
+	  if(EVP_EncryptInit_ex(ctx, EVP_aes_128_ecb(), NULL, key, NULL) != 1){
+		  handleErrors();
+	  }
+	}
 
+
+	if(EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, p_len) != 1){
+	  handleErrors();
+	}
+
+	c_len = len;
+
+
+	if(EVP_EncryptFinal_ex(ctx, ciphertext + len, &len) != 1){
+	  handleErrors();
+	}
+
+	c_len += len;
+
+
+	EVP_CIPHER_CTX_free(ctx);
+
+	return c_len;
 }
 
-
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *plaintext, unsigned char *key, int type)
+int decrypt(unsigned char *ciphertext, int c_len, unsigned char *key, unsigned char *plaintext, int type)
 {
 	EVP_CIPHER_CTX *ctx;
+
 	int len;
-	int plaintext_len;
+	int p_len;
+
 	if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
 
-
 	if(type == cbc){
-		if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, NULL)) handleErrors();
-		if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len)) handleErrors();
-		plaintext_len = len;
-
-	}
-	else if (type == ecb){
-		if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_128_ecb(), NULL, key, NULL)) handleErrors();
-		if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len)) handleErrors();
-		plaintext_len = len;
-
+	  if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, NULL)){
+		  handleErrors();
+	  }
 	}
 
+	else if(type == ecb){
+	  if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_128_ecb(), NULL, key, NULL)){
+		  handleErrors();
+	  }
+	}
 
-	return plaintext_len;
+
+	if(EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, c_len) != 1){
+	  handleErrors();
+	}
+
+	p_len = len;
 
 
+	if(EVP_DecryptFinal_ex(ctx, plaintext + len, &len) != 1){
+	  handleErrors();
+	}
+	p_len += len;
+
+
+	EVP_CIPHER_CTX_free(ctx);
+
+	return p_len;
 }
 
 
@@ -255,12 +281,12 @@ int main (int argc, char **argv)
 	//argv[3] -k option
 	//argv[4] key
 	//argv[5] -ecb or -cbc
-	//argv[6] -e or -d
-	//argb[7] output filename
-	//128bit k: 11234567891123456
+	// DELETE? argv[6] -e or -d
+	// DELETE? argb[7] output filename
+	//k: 00112233445566778889aabbccddeeff
 
-	if (argc < 8) {
-		cout << "Usage: cryptotool -in input -k key -ecb[-cbc] -e[-d] output\n";
+	if (argc < 6) {
+		cout << "Usage: cryptotool -in input -k key -ecb[-cbc]\n";
 		return 0;
 	} else if (strcmp(argv[1], "-in") != 0) {
 		cout << "Usage: cryptotool -in file -k key -ecb[-cbc]\n";
@@ -273,13 +299,7 @@ int main (int argc, char **argv)
 			cout << "Usage: cryptotool -in file -k key -ecb[-cbc]\n";
 			return 0;
 		}
-	} else if (strcmp (argv[6], "-e") != 0 || strcmp (argv[6], "-d") != 0) {
-		if (strcmp (argv[6], "-e") != 0 && strcmp (argv[6], "-d") != 0) {
-			cout << "Usage: cryptotool -in file -k key -ecb[-cbc]\n";
-			return 0;
-		}
 	}
-
 
 	int type;
 	if(strcmp (argv[5], "-cbc") == 0){
@@ -291,61 +311,181 @@ int main (int argc, char **argv)
 
 	unsigned char *key = (unsigned char *) argv[4];
 
-	ERR_load_crypto_strings();
-	OpenSSL_add_all_algorithms();
-	OPENSSL_config(NULL);
 
-	if(strcmp(argv[6], "-e") == 0){
-		//encryption:
-		ifstream myfile;
-		myfile.open (argv[2]);
+	ifstream myfile;
+	myfile.open (argv[2]);
 
-		if (myfile.is_open()){
-			string line;
-			string lines;
-			while ( getline (myfile,line) ){
-			  lines += line;
-			  lines += "\n";
-			}
+	unsigned char * intext1;
+	int intext_len;
 
-			int length = lines.length()*sizeof(char);
-			char *plaintext = new char[length];
-			const char *plaintext_tmp = lines.c_str();
-			strcpy(plaintext,plaintext_tmp);
+	if (myfile.is_open()){
+		string line;
+		string lines;
 
-			unsigned char * plaintext1 = (unsigned char*)plaintext;
-
-			cout << plaintext1;
-
-			int plaintext_len = strlen((char *)plaintext1);
-
-			unsigned char ciphertext[1000] = "";
-
-			encrypt(plaintext1, plaintext_len, ciphertext, key, type);
-			cout << ciphertext;
-
-			ofstream fout;
-			fout.open(argv[7]);
-			fout << ciphertext;
-			fout.close();
-
-			//myfile.close();
-			delete[] plaintext1;
-
-		}
-			else {
-			cout << "Please specify an existing file.\n";
+		while ( getline (myfile,line) ){
+		  lines += line;
+		  lines += '\n';
 		}
 
-	}//end of encryption
+		int length = lines.length()*sizeof(char);
+		char *intext = new char[length];
+		const char *intext_tmp = lines.c_str();
+		strcpy(intext,intext_tmp);
 
-	else{
-		//decryption
+		intext1 = (unsigned char*)intext;
 
+		intext_len = strlen((char *)intext1);
+		myfile.close();
+
+
+
+		ERR_load_crypto_strings();
+		OpenSSL_add_all_algorithms();
+		OPENSSL_config(NULL);
+
+		//if(strcmp(argv[6], "-e") == 0){
+
+			unsigned char ciphertext[1024] = "";
+			int c_len = encrypt (intext1, intext_len, key, ciphertext, type);
+
+			//cout << ciphertext;
+
+		//}
+		//else{
+			unsigned char plaintext[1024] = "";
+			int p_len;
+			//cout << intext1;
+
+			//p_len = decrypt(intext1, intext_len, key, plaintext, type);
+			p_len = decrypt(ciphertext, c_len, key, plaintext, type);
+			plaintext[p_len] = '\0';
+
+			//cout << plaintext;
+
+			cout << "------------Original data from [ " << argv[2] << " ]"<< endl;
+			cout << intext1 << endl;
+
+			cout << "------------Encrypt data with " << argv[5] << endl;
+			cout << ciphertext << endl;
+
+			cout << "------------Decrypt data with " << argv[5] << endl;
+			cout << plaintext << endl;
+
+		//}
 
 	}
+	else {
+		cout << "Please specify an existing file.\n";
+	}
 
-	return 0;
+
+
+
+  EVP_cleanup();
+  ERR_free_strings();
+
+  return 0;
 }
 
 
+
+
+//int main (int argc, char **argv)
+//{
+//	//argv[1] -in
+//	//argv[2] input filename
+//	//argv[3] -k option
+//	//argv[4] key
+//	//argv[5] -ecb or -cbc
+//	//argv[6] -e or -d
+//	//argb[7] output filename
+//	//128bit k: 11234567891123456
+//
+//	if (argc < 8) {
+//		cout << "Usage: cryptotool -in input -k key -ecb[-cbc] -e[-d] output\n";
+//		return 0;
+//	} else if (strcmp(argv[1], "-in") != 0) {
+//		cout << "Usage: cryptotool -in file -k key -ecb[-cbc]\n";
+//		return 0;
+//	} else if (strcmp (argv[3], "-k") != 0){
+//		cout << "Usage: cryptotool -in file -k key -ecb[-cbc]\n";
+//		return 0;
+//	} else if (strcmp (argv[5], "-cbc") != 0 || strcmp (argv[5], "-ecb") != 0) {
+//		if (strcmp (argv[5], "-cbc") != 0 && strcmp (argv[5], "-ecb") != 0) {
+//			cout << "Usage: cryptotool -in file -k key -ecb[-cbc]\n";
+//			return 0;
+//		}
+//	} else if (strcmp (argv[6], "-e") != 0 || strcmp (argv[6], "-d") != 0) {
+//		if (strcmp (argv[6], "-e") != 0 && strcmp (argv[6], "-d") != 0) {
+//			cout << "Usage: cryptotool -in file -k key -ecb[-cbc]\n";
+//			return 0;
+//		}
+//	}
+//
+//
+//	int type;
+//	if(strcmp (argv[5], "-cbc") == 0){
+//		type = cbc;
+//	}
+//	else{
+//		type = ecb;
+//	}
+//
+//	unsigned char *key = (unsigned char *) argv[4];
+//
+//	ERR_load_crypto_strings();
+//	OpenSSL_add_all_algorithms();
+//	OPENSSL_config(NULL);
+//
+//	if(strcmp(argv[6], "-e") == 0){
+//		//encryption:
+//		ifstream myfile;
+//		myfile.open (argv[2]);
+//
+//		if (myfile.is_open()){
+//			string line;
+//			string lines;
+//			while ( getline (myfile,line) ){
+//			  lines += line;
+//			  lines += "\n";
+//			}
+//
+//			int length = lines.length()*sizeof(char);
+//			char *plaintext = new char[length];
+//			const char *plaintext_tmp = lines.c_str();
+//			strcpy(plaintext,plaintext_tmp);
+//
+//			unsigned char * plaintext1 = (unsigned char*)plaintext;
+//
+//			cout << plaintext1;
+//
+//			int plaintext_len = strlen((char *)plaintext1);
+//
+//			unsigned char ciphertext[1000] = "";
+//
+//			encrypt(plaintext1, plaintext_len, ciphertext, key, type);
+//			cout << ciphertext;
+//
+//			ofstream fout;
+//			fout.open(argv[7]);
+//			fout << ciphertext;
+//			fout.close();
+//
+//			//myfile.close();
+//			delete[] plaintext1;
+//
+//		}
+//			else {
+//			cout << "Please specify an existing file.\n";
+//		}
+//
+//	}//end of encryption
+//
+//	else{
+//		//decryption
+//
+//
+//	}
+//
+//	return 0;
+//}
